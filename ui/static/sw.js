6fa53811e77bf3d3b72f5e37fd890f6d651d301f
boubli <bbb.vloger@gmail.com>
2026-06-03 18:34:35 +0100

    feat(ui): add progressive web app (PWA) manifest and service worker

---
+++ ui/static/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'amud-dashboard-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/style.css',
+  '/static/AMUD-logo.png',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
